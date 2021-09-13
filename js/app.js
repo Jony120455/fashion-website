9ae0c938436cd032a94680085521e5250cb7608a
Jony120455 <jonyh0296@gmail.com>
2021-09-13 22:57:53 +0600

    added all style

--- js/app.js
+++ js/app.js
@@ -35,29 +35,28 @@ const addToCart = (id, price) => {
   updateTaxAndCharge();
   updateTotal(); 
   document.getElementById("total-Products").innerText = count;
-  
 };
 
 const getInputValue = (id) => {
   const element = document.getElementById(id).innerText;
   // const converted = parseInt(element);
-  const converted = parseFloat(element);  // Update Floating Number 
+  const converted = parseFloat(element);
   return converted;
 };
 
 // main price update function
 const updatePrice = (id, value) => {
   const convertedOldPrice = getInputValue(id);
-  const convertPrice = parseFloat(value); // Fixed Floating Number 
+  const convertPrice = parseFloat(value);  
   const total = convertedOldPrice + convertPrice;
   // document.getElementById(id).innerText = Math.round(total);
-  document.getElementById(id).innerText = total.toFixed(2);  // Count Decimal Number 
+  document.getElementById(id).innerText = total.toFixed(2);
 };
 
 // set innerText function
 const setInnerText = (id, value) => {
   // document.getElementById(id).innerText = Math.round(value);
-  document.getElementById(id).innerText = value.toFixed(2);  // Inner Decimal Number 
+  document.getElementById(id).innerText = value.toFixed(2);
 };
 
 // update delivery charge and total Tax
